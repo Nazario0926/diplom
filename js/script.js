4c0f15962db10d94d18d8b2d90eac2a5f1e614a2
Nazario0926 <famoes1234@gmail.com>
2025-03-09 22:09:58 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function orderTour() {
+    alert("Заказ тура. Пожалуйста, свяжитесь с нами через WhatsApp или Telegram.");
+}
+
+function selectTour() {
+    alert("Выберите тур из доступных вариантов.");
+}
